37f1f20d530e9bb23e35168f4448406961255425
prachi <guddip515@gmail.com>
2025-06-23 22:13:52 +0530

    Integrate Tailwind CSS and implement authentication state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appWrite/auth";
+import { login, logout } from "./store/authSlice";
 import "./App.css";
+import { Outlet } from "react-router-dom";
+import { Header, Footer } from "./components";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout);
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return (
-    <>
-      <h1>A blog App with AppWrite</h1>
-    </>
-  );
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
